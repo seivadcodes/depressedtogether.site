0af58d8444a8d5861172c5f30e9b8a47d8f2a1fc
Youngleechen <137005469+Youngleechen@users.noreply.github.com>
2026-02-02 14:01:32 +0300

    a

--- android/app/src/main/java/com/depressedtogether/app/MainActivity.java
+++ android/app/src/main/java/com/depressedtogether/app/MainActivity.java
@@ -1,5 +1,34 @@
 package com.depressedtogether.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.webkit.PermissionRequest;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void init() {
+        super.init();
+        getBridge().getWebView().setWebChromeClient(new BridgeWebChromeClient(getBridge()) {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                // Only allow audio and video capture permissions
+                String[] resources = request.getResources();
+                boolean allowed = true;
+                for (String resource : resources) {
+                    if (!(
+                        PermissionRequest.RESOURCE_AUDIO_CAPTURE.equals(resource) ||
+                        PermissionRequest.RESOURCE_VIDEO_CAPTURE.equals(resource)
+                    )) {
+                        allowed = false;
+                        break;
+                    }
+                }
+                if (allowed) {
+                    request.grant(resources);
+                } else {
+                    request.deny();
+                }
+            }
+        });
+    }
+}
